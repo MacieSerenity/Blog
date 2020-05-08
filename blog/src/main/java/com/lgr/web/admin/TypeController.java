37a519f133cea6e7a3fa6587dcc148803f8f0910
MacieSerenity <1594425336@qq.com>
2020-05-08 17:15:14 +0800

    完成添加分分类功能

--- blog/src/main/java/com/lgr/web/admin/TypeController.java
+++ blog/src/main/java/com/lgr/web/admin/TypeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,24 +34,25 @@ public class TypeController {
             Pageable pageable,
             Model model){
         model.addAttribute("page",typeService.listType(pageable));
-        return "admin/types";
+        return "/admin/types";
     }
 
 
     @GetMapping("/types/input")
     public String input(){
-        return "admin/type-Input";
+        return "/admin/type-Input";
     }
 
     @PostMapping("/types")
-    public String post(Type type){
+    public String post(Type type,RedirectAttributes attributes){
         System.out.println(type);
         Type t=typeService.saveType(type);
-//        if (t==null){
-//            System.out.println("保存失败");
-//        }else {
-//            System.out.println("保存成功");
-//        }
+        if (t==null){
+            attributes.addFlashAttribute("message","操作失败");
+
+        }else {
+            attributes.addFlashAttribute("message","操作成功");
+        }
         return "redirect:/admin/types";
     }
 }
